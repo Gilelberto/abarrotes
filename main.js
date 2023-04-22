2be4969aad964264e031160f17cf7e1cb0ede074
Gil C <a348503@uach.mx>
2023-04-21 18:48:28 -0600

    Se agrega un menu y las funcionalidades que faltaban.

--- main.js
+++ main.js
@@ -1,6 +1,82 @@
 const Products = require('./products');
+const fs = require('fs');
 
+const readline = require("readline");
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+
+function read(text){
+    return new Promise((resolve) => {
+        rl.question(text, (respuesta) => {
+          resolve(respuesta);
+        });
+      });
+}
+
+function printMenu(){
+    console.log("=======================");
+    console.log("1.- Existencia mayor a 25");
+    console.log("2.- Existencia menor a 15.");
+    console.log("3.- Listar por precio mayor a 15.50");
+    console.log("4.- Lista de productos con precio mayor a 20.30 y menor a 45.00");
+    console.log("5.- Número de productos agrupados por su clasificación");
+    console.log("99.- Salir");
+    console.log("=======================");
+
+};
+
+async function menu() {
+    let opt = 0;
+    do{
+        printMenu();
+        opt = await read("Ingrese una opción: ");
+        opt = parseInt(opt);
+        switch(opt){
+            case 1:
+                console.log("");
+                console.log(p1.countProductsByExistence(25,'+')); //existencia mayor a 25
+                break;
+            case 2:
+                console.log("");
+                console.log(p1.countProductsByExistence(15,'-')); //existencia menor a 15
+                break;
+            case 3:
+                console.log("");
+                p1.listByClassAndPrice(15.50,'>');
+                break;
+            case 4:
+                p1.listByClassAndRange(20.30,45);
+                break;
+            case 5:
+                p1.countByClass();
+                break;
+            default:
+                break;
+        }
+    }while(opt != 99);
+    rl.close();
+    //console.log(regi);
+  };
+
+  
 let p1 = new Products('./products.mf');
-//console.log(p1.data);
+(async function () {
+    await menu();
+})();
+
+
+
+
+
+
+
+/*/console.log(p1.data);
 console.log(p1.countProductsByExistence(25,'+')); //existencia mayor a 25
 console.log(p1.countProductsByExistence(15,'-')); //existencia menor a 15
+//console.log(p1.groupByClass('vegetales',p1.data)); 
+p1.listByClassAndPrice(15.50,'>');
+p1.listByClassAndRange(20.30,45);
+p1.countByClass();*/
--- products.js
+++ products.js
@@ -1,3 +1,5 @@
+const { Console } = require('console');
+
 class Products{
     constructor(directory){
         this.directory = directory;
@@ -5,7 +7,7 @@ class Products{
         const fs = require('fs');
         const file = fs.readFileSync(directory, 'utf-8').split('\n');
         file.forEach(line => {
-            this.data.push(line.split(",")); 
+            this.data.push(line.split(",").map(x => x.trim())); 
         });
     }
 
@@ -30,9 +32,82 @@ class Products{
         return counter;
     }
 
-    groupBy(){
-        
+    countByClass(){
+        console.log(`=====Conteo por clase=====`);
+        let types = this.getTypes();
+        for(let typ in types){
+            let products = this.groupByClass(types[typ],this.data);        
+            console.log(`Tipo: ${types[typ]}, existen: ${products.length} productos.`);
+        }
     }
+
+    getTypes(){
+        let types = [];
+        for(let i = 0; i<this.data.length;i++){
+            if(!types.includes(this.data[i][1])){
+                types.push(this.data[i][1]);
+            }
+        }
+        return types;
+    }
+    groupByClass(typ,data){
+    
+        let products = [];
+        for(let i = 0; i<data.length;i++){
+            if(data[i][1] == typ){
+                products.push(data[i]);
+            }
+        }
+        return products;
+    }
+
+
+    listByClassAndPrice(price,operator){
+        console.log(`=====Lista de productos con precio ${operator} a ${price}=====`);
+        let types = this.getTypes();
+        for(let typ in types){
+            
+            let products = this.groupByClass(types[typ],this.data);
+            //console.log(products);
+            let finalProducts = [];
+            for(let p in products){
+                if(operator == '>'){
+                    if (products[p][3] >= price ){
+                        finalProducts.push(products[p]);
+                    }
+                }
+                else if (operator == '<'){
+                    if (products[p][3] <= price ){
+                        finalProducts.push(products[p]);
+                    }
+                }
+            }
+            if (finalProducts > []){
+                console.log("Tipo: " + types[typ]);
+                console.log(finalProducts);}
+        }
+    }
+
+    listByClassAndRange(min,max){
+        console.log(`=====Lista de productos con precio entre ${max} y ${min}=====`);
+        let types = this.getTypes();
+        for(let typ in types){
+            
+            let products = this.groupByClass(types[typ],this.data);
+            //console.log(products);
+            let finalProducts = [];
+            for(let p in products){
+                if (products[p][3] >= min && products[p][3] <= max ){
+                    finalProducts.push(products[p]);
+                }
+            }
+            
+            if (finalProducts > []){
+                console.log("Tipo: " + types[typ]);
+                console.log(finalProducts);}
+        }
+    }
+
 }
 
 /*(async () => {
